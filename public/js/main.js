48d7098c8a48a58ba1b32081827f9df60f81ab46
JacobGraham02 <jakegraham54@gmail.com>
2021-11-11 15:12:55 -0500

    Added construction of html5 element div to add to container 'chat-messages' which contains the message user has sent

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,32 @@
+const chatForm = document.getElementById("chat-form");
+
 const socket = io();
 
+// Each time something is sent with an id of {message}, socketio will output this to the console
 socket.on('message', message => {
-    console.log(message);
+    outputMessage(message);
 });
+
+// Event listener to listen for the submission of a message
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault(); // Do not allow anything to submit via the default action of a form
+
+    const msg = e.target.elements.msg.value; // Get the currently targetted element called 'msg' from the form and get the value of the element 
+    
+    // Emit (send) a message called chatMessage to the server 
+    socket.emit('chatMessage', msg);
+});
+
+// Ouput the specified message to the DOM
+/* Create a new html div which contains the message text of client application connection, and append this div to the container "chat-messages" as a child div element
+*/
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+    <p class="text">
+        ${message}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+
+}
--- server.js
+++ server.js
@@ -13,19 +13,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Run the socketio dependency when a client connects to our chat application
 io.on('connection', socket => {
-    console.log("New websocket connection");
-    
     // Emit a message to a single client when they connect to application
     // emit sends a message only to the single client that is connected
     socket.emit('message', 'Welcome to JaGra'); 
 
     // Broadcast when a user connects to the application
     // broadcast.emit sends a message to every client except for the currently connected one
-    socket.broadcast.emit('message', 'a user has joined the chat');
+    socket.broadcast.emit('message', 'A user has joined the chat');
 
     // Runs when client disconnects and sends a message to each client that is connected to the application
     socket.on('disconnect', () => {
         io.emit('message', 'A user has left the chat');
+    });
+
+    // Listen for any outgoing messages with an id of chatMessage. We want this method to send a reponse back to everybody containing the chatMessage text
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', msg);
     });
 });
 
